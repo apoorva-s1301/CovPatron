34685846c0ad2d8e8625e9315ffe9eea24bcc743
Apoorva Singh <apoorvasingh130@gmail.com>
2021-06-06 18:50:49 +0530

    Add initial files.

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res)
+{
+    res.render("home");
+});
+app.get("/about",function(req,res)
+{
+    res.render("about");
+});
+app.get("/ourSupport",function(req,res)
+{
+    res.render("ourSupport");
+});
+app.get("/contact",function(req,res)
+{
+    res.render("contact");
+});
+
+
+app.listen(3000,function(){
+    console.log("Server started on port 3000.");
+});
+
+
+
+
+
+
+
+
